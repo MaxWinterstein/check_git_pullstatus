adbd8606f950df6b86c56d2dac17c419a4daa4d2
Max Winterstein <max@winterstein.mx>
2014-08-21 10:58:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+just a simple code i have thrown together to check if a git repository is up to date. nothing more.
---
+++ check_git_pullstatus.sh
@@ -0,0 +1,20 @@
+# idea from http://stackoverflow.com/a/16920556/635876
+
+#!/bin/bash
+GITNAME="myRepository"
+DIR="/path/to/myRepo"
+
+
+cd $DIR
+if [ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] ; then
+    status=0
+    statustxt="up to date"
+else
+    status=2
+    statustxt="not up to date"
+fi
+
+echo "$status git_status_$GITNAME - $statustxt"
+
+
+
